e9be6b383fac7123317d2b8efae22e937f72920e
khalifa5000 <m.khalifa5000@gmail.com>
2024-06-10 01:24:58 +0300

    Edit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
-document.write("<h1>ELZERO</h1>").style.color= "blue";
-// font-size= "80px";
-// font-weight= "bold";
-// text-align= "center";
-// font-family= "Arial"
+document.write("<h1>ELZERO</h1>");
+document.querySelector("h1").style.color= "blue"
+document.querySelector("h1").style.fontSize = "80px";
+document.querySelector("h1").style.fontWeight = "bold";
+document.querySelector("h1").style.textAlign = "center";
+document.querySelector("h1").style.fontFamily = "Arial";
