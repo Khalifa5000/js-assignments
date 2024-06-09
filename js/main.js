6750779572528d9ae8aeddeb14124a990714af29
khalifa5000 <m.khalifa5000@gmail.com>
2024-06-10 02:02:19 +0300

    Edit

--- js/main.js
+++ js/main.js
@@ -1,6 +1,34 @@
+// التكليف 2
+
 document.write("<h1>ELZERO</h1>");
 document.querySelector("h1").style.color= "blue"
 document.querySelector("h1").style.fontSize = "80px";
 document.querySelector("h1").style.fontWeight = "bold";
 document.querySelector("h1").style.textAlign = "center";
 document.querySelector("h1").style.fontFamily = "Arial";
+
+
+// التكليف 3
+console.log("%cElzero %cWeb %cSchool", "color: red; font-size:40px;", "color: green; font-size:40px; font-weight: bold", "color: white; font-size: 40px; background-color: blue;")
+
+// التكليف 4
+
+console.group("group 1");
+console.log("message one");
+console.log("message two");
+console.group("child group")
+console.log("message one");
+console.log("message two");
+console.group("grand child group")
+console.log("message one");
+console.log("message two");
+console.groupEnd();
+console.groupEnd();
+console.groupEnd();
+console.group("group 2")
+console.log("message one");
+console.log("message two");
+
+
+// التكليف 5
+
